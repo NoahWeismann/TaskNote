998ae97ab44c5d7c221d49b071ecf0f1b311e709
Thomas Trinh <thomastrinh1576@gmail.com>
2022-01-31 00:52:37 -0600

    First commit

---
+++ CLINote.py
@@ -0,0 +1,29 @@
+"
+Command-line tool to remind daily tasks
+"
+#bro wtf is going on man
+
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='check daily tasks')
+    parser.add_argument('task',
+                        help='task to use')
+
+    parser.add_argument('--add', '-a',
+                        help='Add to list',
+                        action='store_true')
+        
+    parser.add_argument('--list', '-l'
+                        help='check to-do list',
+                        action='store_true')
+
+
+    args = parser.parse_args()
+
+    if args.list:
+        with open('todolist.txt','w') as f:
+            f_contents = f.read()
+            print(f_contents)
+
